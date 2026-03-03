cc9571bd97d812e3997b6172f3491954921f0d5c
AashviJariwala <aashvijariwala@gmail.com>
2026-03-03 12:27:41 +0530

    Aashvi's minor changes

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const express = require("express");
-const cors=require("cors");
-const morgan=require("morgan");
-const {errorHandler}=require("./middleware/errorHandler");
+const cors = require("cors");
+const morgan = require("morgan");
+const { errorHandler } = require("./middleware/errorHandler");
+const session = require("express-session");
 require("dotenv").config();
 require("./config/myconn");
 require("./models/role");
 require("./ADMIN/models/admin");
-const adminRoutes=require("./ADMIN/routes/adminRoutes");
-// const loginRoutes = require("./routes/loginRoutes");
-// const passport = require("./auth/passport");
+const adminRoutes = require("./ADMIN/routes/adminRoutes");
+const loginRoutes = require("./routes/loginRoutes");
+const passport = require("./auth/passport");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
 app.use(cors());
-app.use(morgan("dev")); 
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(morgan("dev"));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/admin", adminRoutes);
-// app.use("/login", loginRoutes);
+app.use("/login", loginRoutes);
 app.use(errorHandler);
-
 
 app.listen(3000, () => console.log("Listening on 3000"));
--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,38 +1,28 @@
 const express = require("express");
-const login = require("../controllers/login");
 const myPass = require("../auth/passport");
 
 const router = express.Router();
-
-router.get("/", login.loginForm);
-router.get("/profileForm", login.profileForm);
-router.get("/profile", login.profile);
 
 // 🔥 Google login route (start OAuth)
 router.get(
   "/google",
   myPass.authenticate("google", {
-    scope: [
-      "profile",
-      "https://www.googleapis.com/auth/calendar"
-    ],
+    scope: ["profile", "https://www.googleapis.com/auth/calendar"],
     accessType: "offline",
-    prompt: "consent"
+    prompt: "consent",
   })
 );
 
 // 🔥 Google callback route
 "email",
-router.get(
-  "/auth/google/callback",
-  myPass.authenticate("google", {
-    failureRedirect: "/login"
-  }),
-  (req, res) => {
-    res.redirect("/profile"); // or wherever you want
-  }
-);
-
-router.get("/logout", login.logout);
+  router.get(
+    "/auth/google/callback",
+    myPass.authenticate("google", {
+      failureRedirect: "/login",
+    }),
+    (req, res) => {
+      res.redirect("/profile"); // or wherever you want
+    }
+  );
 
 module.exports = router;
